561abf5739ffcc518a33ea51b92177ee85360e1e
DaiSwap <19644pranavvenkatesh@gmail.com>
2024-06-02 20:18:41 +0530

    validating

--- validation.py
+++ validation.py
@@ -1,43 +1,41 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
-# Load data
 melbourne_file_path = '/home/pv/ml_model/melb_data.csv'
-melbourne_data = pd.read_csv(melbourne_file_path) 
+melbourne_data = pd.read_csv(melbourne_file_path)
+
 # Filter rows with missing price values
 filtered_melbourne_data = melbourne_data.dropna(axis=0)
+
 # Choose target and features
 y = filtered_melbourne_data.Price
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
-                        'YearBuilt', 'Lattitude', 'Longtitude']
+                      'YearBuilt', 'Lattitude', 'Longtitude']
 X = filtered_melbourne_data[melbourne_features]
 
-from sklearn.tree import DecisionTreeRegressor
-# Define model
 melbourne_model = DecisionTreeRegressor()
-# Fit model
+
+# Fit model on the entire dataset (in-sample evaluation)
 melbourne_model.fit(X, y)
 
-
-from sklearn.metrics import mean_absolute_error
-
 predicted_home_prices = melbourne_model.predict(X)
-r = mean_absolute_error(y, predicted_home_prices)
-print("mean:")
-print(r)
-'''this is an insample score, does not work for new data only training data'''
 
-from sklearn.model_selection import train_test_split
+# Calculate the Mean Absolute Error for in-sample predictions
+in_sample_mae = mean_absolute_error(y, predicted_home_prices)
+print("In-sample Mean Absolute Error:")
+print(in_sample_mae)
+print("This is an in-sample score, does not work for new data only training data")
 
-# split data into training and validation data, for both features and target
-# The split is based on a random number generator. Supplying a numeric value to
-# the random_state argument guarantees we get the same split every time we
-# run this script.
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
-# Define model
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+
 melbourne_model = DecisionTreeRegressor()
-# Fit model
 melbourne_model.fit(train_X, train_y)
 
-# get predicted prices on validation data
 val_predictions = melbourne_model.predict(val_X)
-print(mean_absolute_error(val_y, val_predictions))
+
+
+validation_mae = mean_absolute_error(val_y, val_predictions)
+print("Validation Mean Absolute Error:")
+print(validation_mae)
